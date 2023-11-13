81b4f28729c14f5737c1725da6b492be1f20e92f
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-11-13 17:42:54 +0300

    new design

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const downloadedImage = document.querySelector('.result-image');
 const canvasElement = document.getElementById('canvas');
 const hatImage = document.getElementById('hatImage');
 const firstPage = document.querySelector('.first-page');
+const firstPageButton = firstPage.querySelector('.first-page__button');
 const videoContainer = document.querySelector('.main__video-container');
 const resultImage = document.querySelector('.result-image');
 const attachmentPhoto = document.querySelector('.attachment-photo');
@@ -25,6 +26,13 @@ const loadingText = document.querySelector('.loading-neuro__text');
 const secondPage = document.querySelector('.second-page');
 const secondPageInput = secondPage.querySelector('.second-page__input');
 const secondPageButton = secondPage.querySelector('.second-page__button');
+const thirdPage = document.querySelector('.third-page');
+const thirdPageButton = thirdPage.querySelector('.third-page__button');
+const mainPage = document.querySelector('.main-page');
+const mainPageButton = mainPage.querySelector('.main-page__button');
+const finalPage = document.querySelector('.final-page');
+const finalIMG = finalPage.querySelector('.final-page__result-image');
+const finalButton = finalPage.querySelector('.final-page__button');
 
 const botToken = '6899155059:AAEaXDEvMiL7qstq_9BFQ59fEXGo-mcF1hU';
 let userChatId = '';
@@ -41,11 +49,11 @@ if (detect.os() === null) {
     left: 50%;
     transform: translateX(-50%);
   `;
-  resultImage.style = `
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%);
-  `;
+  // resultImage.style = `
+  //   position: relative;
+  //   left: 50%;
+  //   transform: translateX(-50%);
+  // `;
 }
 
 async function loadModels() {
@@ -86,6 +94,11 @@ window.addEventListener('DOMContentLoaded', () => {
   userChatId = user_data["id"];
 });
 
+firstPageButton.addEventListener('click', () => {
+  firstPage.classList.add("first-page_disabled");
+  secondPage.classList.remove("second-page_disabled");
+})
+
 secondPageInput.addEventListener('input', (evt) => {
   if (evt.target.value.trim().length !== 0) {
     secondPageButton.disabled = false;
@@ -94,64 +107,68 @@ secondPageInput.addEventListener('input', (evt) => {
 
 secondPageButton.addEventListener('click', () => {
   secondPage.classList.add('second-page_disabled');
+  thirdPage.classList.remove('third-page_disabled');
 });
 
-sendButton.addEventListener('click', () => {
+// thirdPageButton.addEventListener('click', () => {
+//   thirdPage.classList.add('third-page_disabled');
+// })
 
-    const canvas = document.createElement('canvas');
-    canvas.width = downloadedImage.width; // Ширина вашего изображения
-    canvas.height = downloadedImage.height; // Высота вашего изображения
-    const ctx = canvas.getContext('2d');
+// sendButton.addEventListener('click', () => {
+
+//     const canvas = document.createElement('canvas');
+//     canvas.width = downloadedImage.width; // Ширина вашего изображения
+//     canvas.height = downloadedImage.height; // Высота вашего изображения
+//     const ctx = canvas.getContext('2d');
     
-    // Нарисуйте изображение на Canvas
-    ctx.drawImage(downloadedImage, 0, 0, canvas.width, canvas.height);
+//     // Нарисуйте изображение на Canvas
+//     ctx.drawImage(downloadedImage, 0, 0, canvas.width, canvas.height);
 
 
-    canvas.toBlob(function (blob) {
-    // Формируем объект FormData для отправки файла
-    const formData = new FormData();
-    formData.append('chat_id', userChatId);
-    formData.append('photo', blob, 'photo.png');
+//     canvas.toBlob(function (blob) {
+//     // Формируем объект FormData для отправки файла
+//     const formData = new FormData();
+//     formData.append('chat_id', userChatId);
+//     formData.append('photo', blob, 'photo.png');
   
-    // Формируем URL для отправки фотографии
-    const apiUrl = `https://api.telegram.org/bot${botToken}/sendPhoto`;
+//     // Формируем URL для отправки фотографии
+//     const apiUrl = `https://api.telegram.org/bot${botToken}/sendPhoto`;
   
-    // Отправка фотографии на сервер Telegram
-    fetch(apiUrl, {
-      method: 'POST',
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.ok) {
-          console.log('Фотография успешно отправлена в Telegram.');
-        } else {
-          console.error('Произошла ошибка при отправке фотографии.');
-        }
-      })
-      .catch(error => {
-        console.error('Ошибка:', error);
-      });
-  });
-});
+//     // Отправка фотографии на сервер Telegram
+//     fetch(apiUrl, {
+//       method: 'POST',
+//       body: formData,
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         if (data.ok) {
+//           console.log('Фотография успешно отправлена в Telegram.');
+//         } else {
+//           console.error('Произошла ошибка при отправке фотографии.');
+//         }
+//       })
+//       .catch(error => {
+//         console.error('Ошибка:', error);
+//       });
+//   });
+// });
 
 
-setTimeout(() => {
-  loadingPage.classList.add('loading-page_disabled');
-}, 2500);
-clearTimeout();
+// setTimeout(() => {
+//   loadingPage.classList.add('loading-page_disabled');
+// }, 2500);
+// clearTimeout();
 
-secondPage.classList.remove('second-page_disabled');
+// secondPage.classList.remove('second-page_disabled');
 
 // Функция для получения доступа к камере
 async function startCamera() {
   try {
     stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } });
     videoElement.srcObject = stream;
-    stopCameraButton.disabled = false;
-    startCameraButton.disabled = true;
-    nextButton.disabled = false;
+    thirdPage.classList.add('third-page_disabled');
+    loadingNeuro.classList.remove('loading-neuro_disabled');
     console.log('доступ к камере дан')
   } catch (error) {
     console.error('Ошибка при получении доступа к камере:', error);
@@ -169,12 +186,11 @@ function stopCamera() {
 }
 
 // Назначение обработчиков событий кнопкам
-startCameraFirst.addEventListener('click', () => {
+// startCameraFirst.addEventListener('click', () => {
+//   startCamera();
+// });
+thirdPageButton.addEventListener('click', () => {
   startCamera();
-});
-nextButton.addEventListener('click', () => {
-  firstPage.classList.add('first-page_disabled');
-  loadingNeuro.classList.remove('loading-neuro_disabled');
   startFaceVideoDetection(videoElement, canvas);
   if (detect.os() === 'iOS') {
     stopCamera();
@@ -188,14 +204,12 @@ nextButton.addEventListener('click', () => {
     loadingNeuro.classList.add('loading-neuro_disabled');
   }, 4000);
   clearTimeout();
-})
-startCameraButton.addEventListener('click', startCamera);
-stopCameraButton.addEventListener('click', stopCamera);
+});
 
-// Изначально отключаем кнопку "Выключить камеру"
-stopCameraButton.disabled = true;
+mainPageButton.addEventListener('click', () => {
+  mainPage.classList.add('main-page_disabled');
 
-downloadButton.addEventListener('click', () => {
+
   canvasElement.width = videoElement.videoWidth;
   canvasElement.height = videoElement.videoHeight;
 
@@ -205,90 +219,119 @@ downloadButton.addEventListener('click', () => {
   const canvasContext = canvasElement.getContext('2d');
   canvasContext.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
   const hatX = x;
-  const hatY = y;
+  const hatY = y - hatHeight / 2;
+
   canvasContext.drawImage(hatImage, hatX, hatY, hatWidth, hatHeight);
 
-  downloadedImage.src = canvasElement.toDataURL('image/png');
-  downloadedImage.classList.add('result-image_active');
+  finalIMG.src = canvasElement.toDataURL('image/png');
+
+  finalPage.classList.add('final-page_active');
 });
 
-inputPhoto.onchange = function(event) {
-  canvasElement3.style.opacity = 0;
-  var target = event.target;
-
-  if (!FileReader) {
-      alert('FileReader не поддерживается — облом');
-      return;
-  }
-
-  if (!target.files.length) {
-      alert('Ничего не загружено');
-      return;
-  }
-
-  var fileReader = new FileReader();
-  fileReader.onload = function() {
-    attachmentPhoto.classList.add('attachment-photo_active');
-    attachmentPhoto.src = fileReader.result;
-  }
-
-  fileReader.readAsDataURL(target.files[0]);
-}
-
-maskButton.addEventListener('click', () => {
-  canvasElement3.width = attachmentPhoto.width;
-  canvasElement3.height = attachmentPhoto.height;
-  const canvasContext = canvasElement3.getContext('2d');
-  canvasContext.drawImage(attachmentPhoto, 0, 0, canvasElement3.width, canvasElement3.height);
-  attachmentPhoto.src = canvasElement3.toDataURL('image/png');
-  startFacePhotoDetection(attachmentPhoto, canvas2);
+finalButton.addEventListener('click', () => {
+  finalPage.classList.remove('final-page_active');
+  mainPage.classList.remove('main-page_disabled');
 });
+// startCameraButton.addEventListener('click', startCamera);
+// stopCameraButton.addEventListener('click', stopCamera);
 
-sendAttachButton.addEventListener('click', () => {
-  const canvas = document.createElement('canvas');
-    canvas.width = attachmentPhoto.width; // Ширина вашего изображения
-    canvas.height = attachmentPhoto.height; // Высота вашего изображения
-    const ctx = canvas.getContext('2d');
+// Изначально отключаем кнопку "Выключить камеру"
+// stopCameraButton.disabled = true;
+
+// downloadButton.addEventListener('click', () => {
+//   canvasElement.width = videoElement.videoWidth;
+//   canvasElement.height = videoElement.videoHeight;
+
+//   const hatAspectRatio = hatImage.width / hatImage.height;
+//   const hatHeight = hatWidth / hatAspectRatio;
+
+//   const canvasContext = canvasElement.getContext('2d');
+//   canvasContext.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+//   const hatX = x;
+//   const hatY = y;
+//   canvasContext.drawImage(hatImage, hatX, hatY, hatWidth, hatHeight);
+
+//   downloadedImage.src = canvasElement.toDataURL('image/png');
+//   downloadedImage.classList.add('result-image_active');
+// });
+
+// inputPhoto.onchange = function(event) {
+//   canvasElement3.style.opacity = 0;
+//   var target = event.target;
+
+//   if (!FileReader) {
+//       alert('FileReader не поддерживается — облом');
+//       return;
+//   }
+
+//   if (!target.files.length) {
+//       alert('Ничего не загружено');
+//       return;
+//   }
+
+//   var fileReader = new FileReader();
+//   fileReader.onload = function() {
+//     attachmentPhoto.classList.add('attachment-photo_active');
+//     attachmentPhoto.src = fileReader.result;
+//   }
+
+//   fileReader.readAsDataURL(target.files[0]);
+// }
+
+// maskButton.addEventListener('click', () => {
+//   canvasElement3.width = attachmentPhoto.width;
+//   canvasElement3.height = attachmentPhoto.height;
+//   const canvasContext = canvasElement3.getContext('2d');
+//   canvasContext.drawImage(attachmentPhoto, 0, 0, canvasElement3.width, canvasElement3.height);
+//   attachmentPhoto.src = canvasElement3.toDataURL('image/png');
+//   startFacePhotoDetection(attachmentPhoto, canvas2);
+// });
+
+// sendAttachButton.addEventListener('click', () => {
+//   const canvas = document.createElement('canvas');
+//     canvas.width = attachmentPhoto.width; // Ширина вашего изображения
+//     canvas.height = attachmentPhoto.height; // Высота вашего изображения
+//     const ctx = canvas.getContext('2d');
     
-    // Нарисуйте изображение на Canvas
-    ctx.drawImage(attachmentPhoto, 0, 0, canvas.width, canvas.height);
+//     // Нарисуйте изображение на Canvas
+//     ctx.drawImage(attachmentPhoto, 0, 0, canvas.width, canvas.height);
 
 
-    canvas.toBlob(function (blob) {
-    // Формируем объект FormData для отправки файла
-    const formData = new FormData();
-    formData.append('chat_id', userChatId);
-    formData.append('photo', blob, 'photo.png');
+//     canvas.toBlob(function (blob) {
+//     // Формируем объект FormData для отправки файла
+//     const formData = new FormData();
+//     formData.append('chat_id', userChatId);
+//     formData.append('photo', blob, 'photo.png');
   
-    // Формируем URL для отправки фотографии
-    const apiUrl = `https://api.telegram.org/bot${botToken}/sendPhoto`;
+//     // Формируем URL для отправки фотографии
+//     const apiUrl = `https://api.telegram.org/bot${botToken}/sendPhoto`;
   
-    // Отправка фотографии на сервер Telegram
-    fetch(apiUrl, {
-      method: 'POST',
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.ok) {
-          console.log('Фотография успешно отправлена в Telegram.');
-        } else {
-          console.error('Произошла ошибка при отправке фотографии.');
-        }
-      })
-      .catch(error => {
-        console.error('Ошибка:', error);
-      });
-  });
-});
+//     // Отправка фотографии на сервер Telegram
+//     fetch(apiUrl, {
+//       method: 'POST',
+//       body: formData,
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         if (data.ok) {
+//           console.log('Фотография успешно отправлена в Telegram.');
+//         } else {
+//           console.error('Произошла ошибка при отправке фотографии.');
+//         }
+//       })
+//       .catch(error => {
+//         console.error('Ошибка:', error);
+//       });
+//   });
+// });
 
 async function startFaceVideoDetection(assetElement, canvasElement) {
-  const MODEL_URL = './models';
-  await window.faceapi.loadTinyFaceDetectorModel(MODEL_URL);
-  await window.faceapi.loadFaceLandmarkModel(MODEL_URL);
-  await window.faceapi.loadFaceRecognitionModel(MODEL_URL);
-  await window.faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+  // const MODEL_URL = './models';
+  // await window.faceapi.loadTinyFaceDetectorModel(MODEL_URL);
+  // await window.faceapi.loadFaceLandmarkModel(MODEL_URL);
+  // await window.faceapi.loadFaceRecognitionModel(MODEL_URL);
+  // await window.faceapi.loadSsdMobilenetv1Model(MODEL_URL);
 
   const context = canvasElement.getContext('2d');
 
@@ -303,20 +346,20 @@ async function startFaceVideoDetection(assetElement, canvasElement) {
 
           const leftPoint = leftEyeBrow[0];
           const rightPoint = rightEyeBrow.splice(-1)[0];
-          const width = (rightPoint.x - leftPoint.x) * 2;
+          const width = (rightPoint.x - leftPoint.x) * 2.7;
 
           // const scale = attachmentPhoto.width / attachmentPhoto.naturalWidth
 
           canvasElement.width = width;
           hatWidth = width;
-          canvasElement.height = 91;
+          canvasElement.height = 295;
           canvasElement.style.width = width + 'px';
-          x = (leftPoint.x - width * 0.10) - 10;
-          y = (leftEyeBrow[0].y - width * 0.55);
-          canvasElement.style.left = (leftPoint.x - width * 0.10) - 10 + 'px';
-          canvasElement.style.top = (leftEyeBrow[0].y - width * 0.55) + 'px';
+          x = (leftPoint.x) - 100;
+          y = (leftEyeBrow[0].y + 20);
+          canvasElement.style.left = (leftPoint.x) - 100 + 'px';
+          canvasElement.style.top = (leftEyeBrow[0].y + 20) + 'px';
 
-          context.drawImage(hatImage, 0, 0, canvasElement.width, 91);
+          context.drawImage(hatImage, 0, 0, canvasElement.width, 295);
       });
   }, 100);
 }
